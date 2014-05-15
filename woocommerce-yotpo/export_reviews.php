d31f07c45bb7d8a627776333456d59b930abf1ec
Alon Braitstein <alon@yotpo.com>
2014-05-15 10:17:35 +0300

    make sure user have manage options before downloading all reviews

--- woocommerce-yotpo/export_reviews.php
+++ woocommerce-yotpo/export_reviews.php
@@ -1,9 +1,12 @@
 <?php
 	require( '../../../wp-load.php' );
-	require('classes/class-wc-yotpo-export-reviews.php');
-	$export = new Yotpo_Review_Export();
-	list($file, $errors) = $export->exportReviews();
-	if(is_null($errors)) {
-		$export->downloadReviewToBrowser($file);	
+	if(current_user_can('manage_options')) {
+		require('classes/class-wc-yotpo-export-reviews.php');	
+		$export = new Yotpo_Review_Export();
+		list($file, $errors) = $export->exportReviews();	
+		if(is_null($errors)) {
+			$export->downloadReviewToBrowser($file);	
+		}
 	}
+
 ?>
